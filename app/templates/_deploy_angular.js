580322505d00af9028d51e1e835ecf4f40ed3563
bjyangchen <bjyangchen@corp.netease.com>
2014-10-24 17:08:57 +0800

    deploy:test

--- app/templates/_deploy_angular.js
+++ app/templates/_deploy_angular.js
@@ -7,112 +7,112 @@ var request = require('request');
 var ftpConfig = require('./ftp.json');
 
 function Deploy(){
-    this.client = new Client();
+  this.client = new Client();
+  this.isConnected = false;
+  this.client.on('end',function(){
     this.isConnected = false;
-    this.client.on('end',function(){
-        this.isConnected = false;
-    }.bind(this));
+  }.bind(this));
 }
 
 Deploy.prototype.connect = function(type){
-    var self = this;
-    var deferred = q.defer();
-    if(!this.isConnected) {
-        if(type == 'resource') {
-            pem.createCertificate({}, function (e, r) {
-                self.client.connect(merge(ftpConfig[type], {
-                        secure: true,
-                        secureOptions: {
-                            key: r.clientKey,
-                            cert: r.certificate,
-                            requestCert: true,
-                            rejectUnauthorized: false
-                        }})
-                );
-            });
-        }
-        else{
-            self.client.connect(ftpConfig[type]);
-        }
-        this.client.on('ready', function () {
-            self.isConnected = true;
-            deferred.resolve();
-        });
+  var self = this;
+  var deferred = q.defer();
+  if(!this.isConnected) {
+    if(type == 'resource') {
+      pem.createCertificate({}, function (e, r) {
+        self.client.connect(merge(ftpConfig[type], {
+            secure: true,
+            secureOptions: {
+              key: r.clientKey,
+              cert: r.certificate,
+              requestCert: true,
+              rejectUnauthorized: false
+            }})
+        );
+      });
     }
     else{
-        deferred.resolve();
+      self.client.connect(ftpConfig[type]);
     }
-    return deferred.promise;
+    this.client.on('ready', function () {
+      self.isConnected = true;
+      deferred.resolve();
+    });
+  }
+  else{
+    deferred.resolve();
+  }
+  return deferred.promise;
 };
 
 Deploy.prototype.uploadResource = function(app,resourceTag){
-    var self = this;
-    function basePromise(){
-        var deferred = q.defer();
-        self.client.mkdir(ftpConfig.resource.urlBase+'/'+app,true,function(){
-            deferred.resolve();
-        });
-        return deferred.promise;
-    }
-    return basePromise().then(function(){
-        return q.all(ftpConfig.resource.include.map(function(v,i){
-            var deferred = q.defer();
-            self.client.mkdir(ftpConfig.resource.urlBase+'/'+app+'/'+v,true,function(){
-                fs.readdir('./build/'+v, function (err, files) {
-                    if(files.length > 0) {
-                        files.map(function (w, i) {
-                            fs.statSync('./build/'+v+'/'+w).isFile() && self.client.put('./build/'+v+'/'+w,ftpConfig.resource.urlBase+'/'+app+'/'+v+'/'+w,false,function(e){
-                                request.get('http://61.135.251.132:81/upimage/cleanmain.php?url=http://img'+resourceTag+'.cache.netease.com'+ftpConfig.resource.urlBase+'/'+app+'/'+v+'/'+w,function(error, response, body){
-                                    deferred.resolve();
-                                });
-                            });
-                        });
-                    }
+  var self = this;
+  function basePromise(){
+    var deferred = q.defer();
+    self.client.mkdir(ftpConfig.resource.urlBase+'/'+app,true,function(){
+      deferred.resolve();
+    });
+    return deferred.promise;
+  }
+  return basePromise().then(function(){
+    return q.all(ftpConfig.resource.include.map(function(v,i){
+      var deferred = q.defer();
+      self.client.mkdir(ftpConfig.resource.urlBase+'/'+app+'/'+v,true,function(){
+        fs.readdir('./build/'+v, function (err, files) {
+          if(files.length > 0) {
+            files.map(function (w, i) {
+              fs.statSync('./build/'+v+'/'+w).isFile() && self.client.put('./build/'+v+'/'+w,ftpConfig.resource.urlBase+'/'+app+'/'+v+'/'+w,false,function(e){
+                request.get('http://61.135.251.132:81/upimage/cleanmain.php?url=http://img'+resourceTag+'.cache.netease.com'+ftpConfig.resource.urlBase+'/'+app+'/'+v+'/'+w,function(error, response, body){
+                  deferred.resolve();
                 });
+              });
             });
-            return deferred.promise;
-        }));
-    }).then(
-        function(){
-            self.client.end();
-        }
-    );
+          }
+        });
+      });
+      return deferred.promise;
+    }));
+  }).then(
+    function(){
+      self.client.end();
+    }
+  );
 };
 Deploy.prototype.uploadPage = function(app,type){
-    var self = this;
-    function basePromise(){
-        var deferred0 = q.defer();
-        self.client.mkdir(ftpConfig[type].urlBase+'/'+app+'/views',true,function(e){
-            var dirs = ['./build/','./build/views/'].map(function(d,i){
-                var deferred1 = q.defer();
-                fs.readdir(d, function (err, files) {
-                    var htmls = files.filter(function (h, i) {
-                        return h.substring(h.length - 5, h.length) == '.html';
-                    }).map(function (v, i) {
-                        var deferred = q.defer();
-                        self.client.put('./build/' + d.substring(8) + v, ftpConfig[type].urlBase + '/' + app + '/' + d.substring(8) + v, false, function (e) {
-                            deferred.resolve();
-                        });
-                        return deferred.promise;
-                    });
-                    q.all(htmls).then(function () {
-                        deferred1.resolve();
-                    });
-                });
-                return deferred1.promise;
+  var self = this;
+  function basePromise(){
+    var deferred0 = q.defer();
+    self.client.mkdir(ftpConfig[type].urlBase+'/'+app+'/views',true,function(e){
+      var dirs = [ftpConfig[type].fileDir+'/',ftpConfig[type].fileDir+'/views/'].map(function(d,i){
+        var deferred1 = q.defer();
+        fs.readdir(d, function (err, files) {
+          var htmls = files.filter(function (h, i) {
+            return h.substring(h.length - 5, h.length) == '.html';
+          }).map(function (v, i) {
+            var deferred = q.defer();
+            self.client.put(d + v, ftpConfig[type].urlBase + '/' + app + '/' + d.substring(ftpConfig[type].fileDir.length + 1) + v, false, function (e) {
+              deferred.resolve();
             });
-            q.all(dirs).then(function(){
-                deferred0.resolve();
-            });
-
+            return deferred.promise;
+          });
+          q.all(htmls).then(function () {
+            deferred1.resolve();
+          });
         });
-        return deferred0.promise;
+        return deferred1.promise;
+      });
+      q.all(dirs).then(function(){
+        deferred0.resolve();
+      });
+
+    });
+    return deferred0.promise;
+  }
+  return basePromise().then(
+    function(){
+      self.client.end();
     }
-    return basePromise().then(
-        function(){
-            self.client.end();
-        }
-    );
+  );
 };
 
 module.exports = Deploy;
--- app/templates/_gulpfile_angular.js
+++ app/templates/_gulpfile_angular.js
@@ -1,112 +1,133 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
+var ip = require('ip');
+var open = require('open');
 var config = require('./package.json');
+var ftpConfig = require('./ftp.json');
 var Deploy = require('./deploy');
 
 var resourceTag = 1;
 
 gulp.task('sass', function () {
-    return gulp.src(['app/styles/*.scss','app/styles/*.sass'])
-        .pipe($.sass({style: 'expanded'}))
-        .on('error', function(err){
-            console.error(err.toString());
-            this.emit('end');
-        })
-        .pipe($.autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('app/styles'))
-        .pipe(browserSync.reload({stream:true}));
+  return gulp.src(['app/styles/*.scss','app/styles/*.sass'])
+    .pipe($.sass({style: 'expanded'}))
+    .on('error', function(err){
+      console.error(err.toString());
+      this.emit('end');
+    })
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(browserSync.reload({stream:true}));
 });
 gulp.task('style', function () {
-    return gulp.src('app/styles/*.css')
-        .pipe($.concat('online.css'))
-        .pipe($.csso())
-        .pipe(gulp.dest('build/styles'))
-        .pipe($.size({title:'online.css'}));
+  return gulp.src('app/styles/*.css')
+    .pipe($.concat('online.css'))
+    .pipe($.csso())
+    .pipe(gulp.dest('build/styles'))
+    .pipe($.size({title:'online.css'}));
 });
 gulp.task('script', function () {
-    return gulp.src('app/scripts/*.js')
-        .pipe($.concat('online.js'))
-        .pipe($.uglify())
-        .pipe(gulp.dest('build/scripts'))
-        .pipe($.size({title:'online.js'}));
+  return gulp.src('app/scripts/*.js')
+    .pipe($.concat('online.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('build/scripts'))
+    .pipe($.size({title:'online.js'}));
 });
 gulp.task('html', function () {
-    var assets = $.useref.assets();
+  var assets = $.useref.assets();
 
-    return gulp.src('app/*.html')
-        .pipe(assets)
-        .pipe($.if('*.js', $.uglify()))
-        .pipe($.if('*.css', $.csso()))
-        .pipe(assets.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('build'));
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest('build'));
+});
+gulp.task('local',function(){
+  var ipAddress = ip.address();
+  gulp.src('app/*.html')
+    .pipe(gulp.dest('test/'))
+    .pipe($.replace('src="bower_components','src="http://'+ipAddress+'/bower_components'))
+    .pipe($.replace('src="scripts','src="http://'+ipAddress+'/scripts'))
+    .pipe($.replace('href="styles','href="http://'+ipAddress+'/styles'))
+    .pipe($.replace('./images','http://'+ipAddress+'/images'))
+    .pipe(gulp.dest('test/'));
+  gulp.src('app/views/*.html')
+    .pipe(gulp.dest('test/views/'))
+    .pipe($.replace('../images','http://'+ipAddress+'/images'))
+    .pipe(gulp.dest('test/views/'))
 });
 gulp.task('browser-sync', function () {
-    return gulp.task('browser-sync', function() {
-        browserSync({
-            server: {
-                baseDir: "./app",
-                index: "index.html"
-            },
-            browser: 'google chrome',
-            open: "external",
-            port: 80,
-            ghostMode: {
-                clicks: true,
-                location: true,
-                forms: true,
-                scroll: true
-            }
-        });
+  return gulp.task('browser-sync', function() {
+    browserSync({
+      server: {
+        baseDir: "./app",
+        index: "index.html"
+      },
+      browser: 'google chrome',
+      open: "external",
+      port: 80,
+      ghostMode: {
+        clicks: true,
+        location: true,
+        forms: true,
+        scroll: true
+      }
     });
+  });
 });
 gulp.task('watch',function(){
-    gulp.watch([
-        'app/styles/*.css',
-        'app/scripts/*.js',
-        'app/views/*.html'
-    ], function(event) {
-        return gulp.src(event.path)
-            .pipe(browserSync.reload({stream:true}));
-    });
-    gulp.watch(['app/styles/*.scss','app/styles/*.sass'],['sass']);
+  gulp.watch([
+    'app/styles/*.css',
+    'app/scripts/*.js',
+    'app/views/*.html',
+    'app/*.html'
+  ], function(event) {
+    return gulp.src(event.path)
+      .pipe(browserSync.reload({stream:true}));
+  });
+  gulp.watch(['app/styles/*.scss','app/styles/*.sass'],['sass']);
 });
 gulp.task('server', ['browser-sync','watch'], function () {
 
 });
 gulp.task('build',['html'],function(){
-    resourceTag = Math.floor(Math.random()*6)+1;
-    gulp.src('app/views/*.html')
-        .pipe(gulp.dest('build/views'));
-    gulp.src(['build/index.html'])
-        .pipe($.replace('../build/scripts/lib.js', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/scripts/lib.js?v='+new Date().getTime()))
-        .pipe($.replace('../build/scripts/online.js', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/scripts/online.js?v='+new Date().getTime()))
-        .pipe($.replace('../build/styles/online.css', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/styles/online.css?v='+new Date().getTime()))
-        .pipe(gulp.dest('build/'))
+  resourceTag = Math.floor(Math.random()*6)+1;
+  gulp.src('app/views/*.html')
+    .pipe(gulp.dest('build/views'));
+  gulp.src(['build/index.html'])
+    .pipe($.replace('../build/scripts/lib.js', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/scripts/lib.js?v='+new Date().getTime()))
+    .pipe($.replace('../build/scripts/online.js', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/scripts/online.js?v='+new Date().getTime()))
+    .pipe($.replace('../build/styles/online.css', 'http://img'+resourceTag+'.cache.netease.com/utf8/'+config.name+'/styles/online.css?v='+new Date().getTime()))
+    .pipe(gulp.dest('build/'))
 });
 
-gulp.task('deploy_resource',['build'],function(){
-    var deploy_resource = new Deploy();
-    deploy_resource.connect('resource')
-        .then(function(){
-            return deploy_resource.uploadResource(config.name,resourceTag);
-        });
+gulp.task('deploy:resource',['build'],function(){
+  var deploy_resource = new Deploy();
+  deploy_resource.connect('resource')
+    .then(function(){
+      return deploy_resource.uploadResource(config.name,resourceTag);
+    });
 });
-gulp.task('deploy_pageTest',['build'],function(){
-    var deploy_page = new Deploy();
-    deploy_page.connect('pageTest')
-        .then(function(){
-            return deploy_page.uploadPage(config.name,'pageTest');
-        });
+gulp.task('deploy:test',['local'],function(){
+  var deploy_page = new Deploy();
+  deploy_page.connect('test')
+    .then(function(){
+      return deploy_page.uploadPage(config.name,'test');
+    })
+    .then(function(){
+      open(ftpConfig['test'].locationBase+config.name+'/index.html','google chrome');
+    });
 });
-gulp.task('deploy_pageOnline',['build'],function(){
-    var deploy_page = new Deploy();
-    deploy_page.connect('pageOnline')
-        .then(function(){
-            return deploy_page.uploadPage(config.name,'pageOnline');
-        });
+gulp.task('deploy:online',['build'],function(){
+  var deploy_page = new Deploy();
+  deploy_page.connect('online')
+    .then(function(){
+      return deploy_page.uploadPage(config.name,'online');
+    });
 });
